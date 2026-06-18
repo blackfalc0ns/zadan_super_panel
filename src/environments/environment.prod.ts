c8e39d544832015011f8019a9a49c4c9b4ed7727
Abanoub Nabil <AbanoubNabil120@outlook.com>
2026-06-18 15:43:34 +0300

    fix(security): update Content-Security-Policy and API URL in configuration files
    - Modified the Content-Security-Policy in both netlify.toml and vercel.json to replace the old API endpoint with the new one, ensuring proper security measures are in place.
    - Updated the API URL in environment.prod.ts to reflect the new endpoint, maintaining consistency across the application.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://zadana.runasp.net/api',
+    apiUrl: 'https://api.zadna0.com/api',
     skipAuthForDevelopment: false,
     oneSignalAdminAppId: 'c32e801d-7fa0-46f3-bd0a-564af78dbddf',
     oneSignalAllowLocalhost: false
